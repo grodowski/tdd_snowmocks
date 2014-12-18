a5e71eb5cac7164b3820eaf012dc39385b18c0b0
Jan Grodowski <jgrodowski@gmail.com>
2014-12-18 17:02:43 +0100

    placki

--- lib/snow_rescue_service.rb
+++ lib/snow_rescue_service.rb
@@ -1,5 +1,10 @@
-class SnowRescueSvc  
+class SnowRescueSvc
+  def initialize(weather_svc, municipal_svc)
+    @weather_svc = weather_svc
+    @municipal_svc = municipal_svc
+  end
+
   def check_and_perform_rescue
-    raise NotImplementedError.new
+    @municipal_svc.send_sander if @weather_svc.avg_temperature_celcius < 0
   end
 end
--- spec/snowmocks_spec.rb
+++ spec/snowmocks_spec.rb
@@ -7,6 +7,29 @@ require_relative '../lib/dependencies/snowplow_malfunctioning_error'
 # rspec-mocks documentation
 # https://github.com/rspec/rspec-mocks
 
-describe SnowRescueSvc do 
-  
+describe SnowRescueSvc do
+
+  let(:temperature) { 0 }
+  let(:weather_svc) { instance_double('WeatherSvc', avg_temperature_celcius: temperature) }
+  let(:municipal_svc) { instance_double('MunicipalSvc') }
+
+  subject { SnowRescueSvc.new(weather_svc, municipal_svc) }
+
+  context 'when temperature is lt 0' do
+    let(:temperature) { -2 }
+    it 'sends a sander' do
+      expect(municipal_svc).to receive(:send_sander)
+      subject.check_and_perform_rescue
+    end
+  end
+
+  context 'when temperature is gt 0' do
+    let(:temperature) { 2 }
+    it 'does not send a sander ' do
+      expect(municipal_svc).not_to receive(:send_sander)
+      subject.check_and_perform_rescue
+    end
+  end
+
+
 end
